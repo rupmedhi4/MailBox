5b78595a8356b40817c1bfb475ff0afeb1318510
Rup medhi <rupmmedhi@gmail.com>
2023-07-28 21:34:37 +0530

    add route & navigate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Signup from "./Components/Signup/Signup";
 import Login from "./Login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Signup/>
-      <Login/>
+    <div>
+     <BrowserRouter>
+        <Navbar/>
+
+        <Routes>
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
